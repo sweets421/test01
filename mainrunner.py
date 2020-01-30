13b3b6c7047ef6860cccf310b7cbf28e8009af79
sweets421 <943820638@qq.com>
2020-01-30 22:49:23 +0800

    web自动化框架基本搭建完成

--- mainrunner.py
+++ mainrunner.py
@@ -1,9 +1,11 @@
 # coding=utf-8
 from modules.http import HTTP
+from modules.browser import Browser
 from common.Excel import *
 from common import logger, config
 from common.excelresult import Res
 from common.mail import Mail
+from common.mysql import Mysql
 
 import time
 import inspect
@@ -15,10 +17,10 @@ powered by Jhx at 2020/1/24
 
 
 def runcase(line, http):
-    global starttime,endtime
-    starttime=time.asctime(time.localtime(time.time()))
+    global starttime, endtime
+    starttime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
     if len(line[0]) > 0 or len(line[1]) > 0:  # 分组信息，不用执行
-        endtime = time.asctime(time.localtime(time.time()))
+        endtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
         return
     func = getattr(http, line[3])  # 反射获取函数
     args = inspect.getfullargspec(func).__str__()  # 反射获取关键字参数
@@ -28,48 +30,54 @@ def runcase(line, http):
     # 若函数没有参数，就返回
     if len(args) == 0:
         func()
-        endtime = time.asctime(time.localtime(time.time()))
+        endtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
         return
     # 若函数只有一个参数，就读取Excel表格中第五列的入参
     elif len(args) == 1:
         func(line[4])
-        endtime = time.asctime(time.localtime(time.time()))
+        endtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
         return
     # 若函数有2个参数，就读取Excel表格中第五列的入参
     elif len(args) == 2:
         func(line[4], line[5])
-        endtime = time.asctime(time.localtime(time.time()))
+        endtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
         return
     # 若函数有3个参数，就读取Excel表格中第五列的入参
     elif len(args) == 3:
         func(line[4], line[5], line[6])
-        endtime = time.asctime(time.localtime(time.time()))
+        endtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
         return
     else:
         logger.warn("目前最多支持3个参数！")
-        endtime = time.asctime(time.localtime(time.time()))
+        endtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
 
 
-
-
-
-
-
-
-
-
-
-case_path = './lib/HTTP接口用例.xls'
-result_path = './lib/result-HTTP接口用例.xls'
-conf_path='./lib/conf.properties'
+case_path = './lib/Web用例.xls'
+result_path = './lib/result-Web用例.xls'
+conf_path = './lib/conf.properties'
 
 reader = Reader()
 reader.open_excel(case_path)
 sheetnames = reader.get_sheets()
 
+#读取配置文件
+config.get_config(conf_path)
+logger.info(config.config)
+
+#数据库初始化
+# mysql=Mysql()
+# mysql.init_mysql(sql_path)
+
 writer = Writer()
 writer.copy_open(case_path, result_path)
-http = HTTP(writer)
+
+reader.readline()
+http = None
+casetype = reader.readline()[1]
+if casetype == 'HTTP':
+    http = HTTP(writer)
+if casetype == 'WEB':
+    http = Browser(writer)
 
 for sheet in sheetnames:
     reader.set_sheet(sheet)
@@ -95,15 +103,15 @@ logger.info(config.config)
 html = config.config['mailtxt']
 html = html.replace('title', r['title'])
 html = html.replace('status', r['status'])
-if r['status']=="FAIL":
-    html=html.replace('#00d800','red')
+if r['status'] == "Fail":
+    html = html.replace('#00d800', 'red')
 html = html.replace('runtype', r['runtype'])
 html = html.replace('passrate', r['passrate'])
 html = html.replace('casecount', r['casecount'])
 html = html.replace('starttime', str(starttime))
 html = html.replace('endtime', str(endtime))
 
-#发送邮件
+# 发送邮件
 config.get_config(conf_path)
 logger.debug(config.config)
 mail = Mail()
--- modules/browser.py
+++ modules/browser.py
@@ -1,14 +1,26 @@
 # coding=utf-8
 from selenium.webdriver import *
-import time, os
+from selenium.webdriver.common.action_chains import ActionChains
+from common import logger
+import os, time, traceback
 
 
 class Browser:
-    def __init__(self):
-        self.driver = None
+    """
+    powered by Jhx at 2020/1/29
+    用于web自动化的关键字库
+    """
 
-    def openbrowser(self, browser='Chrome', driver=None):
-        if (browser == 'Chrome'):
+    def __init__(self, w):
+        self.driver = None
+        self.title = ''
+        self.text = ''
+        self.writer = w
+
+    def openbrowser(self, browser='Chrome', dr=None):
+        if browser == 'Chrome' or browser == '':
+            if dr == None or dr == '':
+                dr = "./lib/chromedriver.exe"
             # 创建一个用来配置Chrome属性的变量
             Options = ChromeOptions()
             # 去掉"Chrome正受到自动测试软件的控制"字样
@@ -18,20 +30,122 @@ class Browser:
             Options.add_argument(
                 '--user-data-dir=' + user_profile + '\\AppData\\Local\\Google\\Chrome\\User Data\\')
             # 打开谷歌浏览器
-            self.driver = Chrome(executable_path="../lib/chromedriver.exe", options=Options)
-        elif (browser == 'Firefox'):
+            self.driver = Chrome(executable_path=dr, options=Options)
+            self.writer.write(self.writer.row, 7, 'PASS')
+            self.writer.write(self.writer.row, 8, 'Chrome浏览器打开成功')
+        elif browser == 'Firefox':
+            if dr == None or dr == '':
+                dr = '../lib/geckodriver.exe'
             # 打开火狐浏览器
-            self.driver = Firefox(executable_path="../lib/geckodriver.exe")
-        elif (browser == 'ie'):
+            self.driver = Firefox(executable_path=dr)
+            self.writer.write(self.writer.row, 7, 'PASS')
+            self.writer.write(self.writer.row, 8, 'Firefox浏览器打开成功')
+        elif browser == 'ie':
+            if dr == None or dr == '':
+                dr = '../lib/IEDriverServer.exe'
             # 打开ie浏览器
-            self.driver = Ie(executable_path='../lib/IEDriverServer.exe')
+            self.driver = Ie(executable_path=dr)
+            self.writer.write(self.writer.row, 7, 'PASS')
+            self.writer.write(self.writer.row, 8, 'IE浏览器打开成功')
         else:
-            print("该浏览器暂未实现自动化功能!")
+            logger.warn("该浏览器暂未实现自动化功能!")
+            self.writer.write(self.writer.row, 7, 'FAIL')
+            self.writer.write(self.writer.row, 8, '该浏览器暂未实现自动化功能')
+        return
 
     def geturl(self, url):
         # 访问指定网页
-        self.driver.get(url)
+        try:
+            self.driver.get(url)
+            self.writer.write(self.writer.row, 7, 'PASS')
+            self.writer.write(self.writer.row, 8, '网站访问成功')
+        except Exception as e:
+            self.writer.write(self.writer.row, 7,'FAIL')
+            self.writer.write(self.writer.row, 8,traceback.format_exc())
+
+    def wait(self, t=10):
+        if t=='':
+            t=10
+        self.driver.implicitly_wait(t)
+        self.writer.write(self.writer.row, 7, 'PASS')
+        self.writer.write(self.writer.row, 8, '')
+
+    def sleep(self, t=3):
+        if t=='':
+            t=3
+        time.sleep(t)
+        self.writer.write(self.writer.row, 7, 'PASS')
+        self.writer.write(self.writer.row, 8, '')
+
+    def swithwindow(self, idx=0):
+        h = self.driver.window_handles
+        self.driver.switch_to.window(h[int(idx)])
+        self.writer.write(self.writer.row, 7, 'PASS')
+        self.writer.write(self.writer.row, 8, '切换窗口成功')
+
+    def gettitle(self):
+        # 获取窗口名称
+        self.title = self.driver.title
+        self.writer.write(self.writer.row, 7, 'PASS')
+        self.writer.write(self.writer.row, 8, '窗口名称获取成功')
+
+    def closewindow(self):
+        self.driver.close()
+        self.writer.write(self.writer.row, 7, 'PASS')
+        self.writer.write(self.writer.row, 8, '窗口关闭成功')
+
+    def click(self, xpath):
+        self.driver.find_element_by_xpath(xpath).click()
+        self.writer.write(self.writer.row, 7, 'PASS')
+        self.writer.write(self.writer.row, 8, '点击操作成功')
+
+    def input(self, xpath, value):
+        self.driver.find_element_by_xpath(xpath).send_keys(str(value))
+        self.writer.write(self.writer.row, 7, 'PASS')
+        self.writer.write(self.writer.row, 8, '输入操作成功')
+
+    def moveto(self, xpath):
+        actions = ActionChains(self.driver)
+        actions.move_to_element(self.driver.find_element_by_xpath(xpath)).perform()
+        self.writer.write(self.writer.row, 7, 'PASS')
+        self.writer.write(self.writer.row, 8, '滑动操作成功')
+
+    def executejs(self, js):
+        self.driver.execute_script(js)
+        self.writer.write(self.writer.row, 7, 'PASS')
+        self.writer.write(self.writer.row, 8, '滑动操作成功')
+
+    def gettext(self, xpath):
+        self.text = self.driver.find_element_by_xpath(xpath).text
+        self.writer.write(self.writer.row, 7, 'PASS')
+        self.writer.write(self.writer.row, 8, '获得文本成功')
+
+    def intoiframe(self, xpath):
+        self.driver.switch_to.frame(self.driver.find_element_by_xpath(xpath))
+        self.writer.write(self.writer.row, 7, 'PASS')
+        self.writer.write(self.writer.row, 8, '进入iframe成功')
+
+    def outiframe(self):
+        self.driver.switch_to.default_content()
+        self.writer.write(self.writer.row, 7, 'PASS')
+        self.writer.write(self.writer.row, 8, '回到主frame成功')
+
+    def assertequals(self, key, value):
+        try:
+            key = key.replace('{text}', self.text)
+            key = key.replace('{title}', self.title)
+            if str(key) == str(value):
+                self.writer.write(self.writer.row, 7, 'PASS')
+                self.writer.write(self.writer.row, 8, '校验成功')
+            else:
+                self.writer.write(self.writer.row, 7, 'FAIL')
+                self.writer.write(self.writer.row, 8, '校验失败')
+        except Exception as e:
+            self.writer.write(self.writer.row, 7, 'FAIL')
+            self.writer.write(self.writer.row, 8, traceback.format_exc())
 
     def quit(self):
         # 关闭浏览器
         self.driver.quit()
+        self.writer.write(self.writer.row, 7, 'PASS')
+        self.writer.write(self.writer.row, 8, '浏览器关闭成功')
